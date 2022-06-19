ce6734f26119f4e3a491fee9572061e247bfd9d9
shuvomh2000 <shuvomh2000@gmail.com>
2022-06-19 23:07:05 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {Form,Container,Button} from "react-bootstrap"
+import axios from "axios";
 
 function App() {
+
+  const [brand, setBrand] = useState('')
+  const [model, setModel] = useState('')
+  const [price, setPrice] = useState('')
+
+  let handleSubmit = async (e)=>{
+    e.preventDefault()
+    let data =await axios.post("http://localhost:8000/")
+    console.log(data)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Container>
+   <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Brand Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Brand Name" onChange={(e)=>setBrand(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Model Code</Form.Label>
+        <Form.Control type="text" placeholder="Enter Model" onChange={(e)=>setModel(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Price</Form.Label>
+        <Form.Control type="number" placeholder="Enter price" onChange={(e)=>setPrice(e.target.value)}/>
+      </Form.Group>
+      <Button className="w-100" variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+   </Container>
+   </>
   );
 }
 
